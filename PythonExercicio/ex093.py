5209e7ab0d8e179602b2915572066a0488c684ba
Maria Fernanda Vaz <vazfernandam@gmail.com>
2020-07-12 01:42:05 -0300

    12-07-20

--- PythonExercicio/ex093.py
+++ PythonExercicio/ex093.py
@@ -1,18 +1,24 @@
 #Crie um programa quue gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e
 #quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será
 #guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
-print('-' * 45)
-print(f'{"CADASTRO JOGADOR DE FUTEBOL":^45}')
-print('-' * 45)
-dic = dict()
-gols = 0
-dic['nome'] = str(input('Nome do jogador: '))
-dic['partidas'] = int(input(f'Quantas partidas {dic["nome"]} jogou? '))
-for c in range(0, dic['part']):
-    dic['qtd_gols'] = int(input(f'Quantos gols {dic["nome"]} fez na {c+1}° partida? '))
-    gols += dic['qtd_gols']
-print('-' * 45)
-print(dic)
-print('-' * 45)
-for k, v in dic.items():
-    print(f'O campo {k.upper()} tem o valor {v}')
+print('-' * 50)
+print(f'{"CADASTRO JOGADOR DE FUTEBOL":^50}')
+print('-' * 50)
+jogador = dict()
+partidas = list()
+jogador['nome'] = str(input('Nome do Jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, tot):
+    partidas.append(int(input(f'Quantos gols {jogador["nome"]} fez na {c+1}° partida? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-' * 50)
+print(jogador)
+print('-' * 50)
+for k, v in jogador.items():
+    print(f'{k.upper()}: {v}')
+print('-' * 50)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador['gols']):
+    print(f'-> Na {i+1}° partida fez {v} gols.')
+print(f'Foi um total de {jogador["total"]}.')
--- PythonExercicio/ex094.py
+++ PythonExercicio/ex094.py
@@ -1,5 +1,38 @@
 #Crie um programa que leia, nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário
 #e todos os dicionários em uma lista. No final, mostre: a) quantas pessoas foram cadastradas; b)a media de idade do
 #grupo; c)uma lista com todas as mulheres; d)uma lista com todas as pessoas com idade acima da média.
-lista = []
-
+galera = list()
+pessoa = dict()
+soma = 0
+while True:
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('\033[31mERRO! Por favor digite M ou F.\033[m')
+    pessoa['idade'] = int(input(f'Qual a idade de {pessoa["nome"]}? '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Deseja cadastrar mais pessoas? [S/N] ')).upper()[0]
+        print('-' * 40)
+        if resp in 'SN':
+            break
+        print('\033[31mERRO! Responda apenas S ou N.\033[m')
+    if resp == 'N':
+        break
+print(f'Ao todo foram cadastradas {len(galera)} pessoas.')
+print(f'A média de idade do grupo é {soma/len(galera):5.1f} anos.')
+print(f'As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'[{p["nome"]}] ', end='')
+print()
+print('Pessoas que estão acima da média de idade: ')
+for p in galera:
+    if p['idade'] > soma/len(galera):
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
