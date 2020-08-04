cbd42e07e38d6678ac597ae962fa6cd5204d2010
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:09:55 -0300

    Média aritmética

--- PythonExercicio/ex007.py
+++ PythonExercicio/ex007.py
@@ -1,5 +1,5 @@
 #desenvolva um programa que leia duas notas de um aluno, calcule e mostre a média
-print('\033[31m---MÉDIA DAS NOTAS---\033[m') #título
+print('\033[31m----MÉDIA DAS NOTAS----\033[m') #título
 nota1 = float(input('Insira a nota do primeiro semestre: ')) #input da primeira nota para a varivel nota1
 nota2 = float(input('Insira a nota do segundo semestre: ')) #input da segunda nota para a variavel nota2
 print(f'A média semestral desse aluno é \033[31m{(nota1 + nota2) / 2:.1f}\033[m') #resultado da média obtido somando os dois inputs e dividindo por dois
