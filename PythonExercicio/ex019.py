3ff7091d1ef61c80a66e2a176db04a0b1e699bbe
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:34:40 -0300

    Sorteando um item na lista

--- PythonExercicio/ex019.py
+++ PythonExercicio/ex019.py
@@ -2,7 +2,7 @@
 #aula 08, utlizando módulos
 from random import choice
 from time import sleep
-print('\033[1;31m---SORTEIO---\033[m') #titúlo
+print('\033[31m-----------SORTEIO-----------\033[m') #titúlo
 a1 = str(input('Primeiro(a) aluno(a): ')) #input do nome do primeiro aluno
 a2 = str(input('Segundo(a) aluno(a): ')) #input do noome do segundo aluno
 a3 = str(input('Terceiro(a) aluno(a): ')) #input do nome do terceiro aluno
@@ -12,4 +12,4 @@ escolhido = choice(lista) #comp escolhe um nome da lista
 print('\033[31mPROCESSANDO...\033[m')
 sleep(1)
 print('O aluno(a) escolhido para apagar o quadro foi \033[1;31m{}\033[m.'.format(escolhido)) #mostra o aluno escolhido
-print('\033[1;31m-\033[m' * 13)
+print('\033[1;31m-\033[m' * 30)
