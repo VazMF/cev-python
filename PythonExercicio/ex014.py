6776b7c48c4e9ffa3242fdde108d2e78735b2a27
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:24:07 -0300

    Conversor de temperatura

--- PythonExercicio/ex014.py
+++ PythonExercicio/ex014.py
@@ -1,6 +1,6 @@
 #Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 from time import sleep #import do sleep
-print('\033[36m---CONVERSOR DE TEMPERATURA---\033[m') #titulo
+print('\033[36m----------CONVERSOR DE TEMPERATURA----------\033[m') #titulo
 C = int(input('Digite uma temperatura em graus celsius: ')) #input da temperatura em celsius na variavel C
 F = ((9 * C) / 5) + 32 #convertento a temperatura em celsius do input para fahrenheit na varivel F
 print('\033[36mPROCESSANDO...\033[m') #print do processamento para simular loading
