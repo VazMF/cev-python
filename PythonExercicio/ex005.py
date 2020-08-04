2932d63052331348ae0e60a0b561f34c5e8e9d1b
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:06:30 -0300

    Antecessor e sucessor

--- PythonExercicio/ex005.py
+++ PythonExercicio/ex005.py
@@ -1,7 +1,7 @@
 #faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
 #operadores aritiméticos aula 07
 from time import sleep #importando o sleep
-print('\033[31m---ANTECESSOR E SUCESSOR---\033[m') #titulo em vermelho
+print('\033[31m----ANTECESSOR E SUCESSOR----\033[m') #titulo em vermelho
 n = int(input('Insira um número: ')) #variavel do input do numero a ser analisado
 print('\033[33mPROCESSANDO...\033[m') #print da para simulacao do processamento
 sleep(1) #sleep
