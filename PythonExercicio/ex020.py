71b31efc083665e9409090ecbe2ce66a8cee7362
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:37:22 -0300

    Sorteando uma ordem na lista

--- PythonExercicio/ex020.py
+++ PythonExercicio/ex020.py
@@ -1,7 +1,7 @@
 #o mesmo professor quer sortear a ordem de apresentação dos trabalhos dos alunos. faça um programa que leia o nome e mostre a ordem sorteada
 from random import shuffle #importando a função shuffle para escolher a lista
 from time import sleep #importando a função sleep
-print('\033[33m-----ORDEM DE APRESENTAÇÃO-----\033[m') #titúlo
+print('\033[33m------ORDEM DE APRESENTAÇÃO------\033[m') #titúlo
 a1 = str(input('Primeiro(a) aluno(a): ')) #input do primeiro aluno na variavel a1
 a2 = str(input('Segundo(a) aluno(a): ')) #input segundo aluno na variavel a2
 a3 = str(input('Terceiro(a) aluno(a): ')) #input terceiro aluno na variavel a3
