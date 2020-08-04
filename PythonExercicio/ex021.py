caf7f9cf35a2794cf04e27d709cc387fe9561882
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:38:51 -0300

    Tocando um MP3

--- PythonExercicio/ex021.py
+++ PythonExercicio/ex021.py
@@ -1,4 +1,4 @@
-#faça um programa que abra e reproduza um arquivo mp3
+#faça um programa que abra e reproduza um arquivo mp3 (nao esta funcionando)
 import pygame
 pygame.mixer.init()
 pygame.mixer.music.load('ex021.mp3')
