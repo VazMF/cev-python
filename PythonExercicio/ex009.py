510227a099f3838ef02441a145a9d92050c776fd
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:12:44 -0300

    Tabuada

--- PythonExercicio/ex009.py
+++ PythonExercicio/ex009.py
@@ -1,6 +1,6 @@
 #faça um programa que leia um número inteiro e mostre sua tabuada
 from time import sleep #import do sleep
-print('\033[1;31m------TABUADA------\033[m') #titúlo
+print('\033[1;31m-------TABUADA-------\033[m') #titúlo
 n = int(input('Insira um número: ')) #input do numero na variavel num
 print(f'{n} x 1 \033[31m=\033[m {n*1}') #print da visualizacao e multiplicacao dos numeros
 print(f'{n} x 2 \033[31m=\033[m {n*2}')
@@ -12,4 +12,4 @@ print(f'{n} x 7 \033[31m=\033[m {n*7}')
 print(f'{n} x 8 \033[31m=\033[m {n*8}')
 print(f'{n} x 9 \033[31m=\033[m {n*9}')
 print(f'{n} x 10 \033[31m=\033[m {n*10}')
-print('\033[31m-\033[m'*20) #print do final
+print('\033[31m-\033[m'*22) #print do final
