289bb50afd71d61adf2b3d3853610f161c8384bc
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:02:09 -0300

    Respondendo ao usuário

--- PythonExercicio/ex002.py
+++ PythonExercicio/ex002.py
@@ -1,3 +1,3 @@
-print('\033[1;31m---NICE TO MEET YOU---\033[m') #titulo do programa
+print('\033[1;31m----NICE TO MEET YOU----\033[m') #titulo do programa
 nome = input('\033[1mDigite seu nome\033[m: ') #variavel para receber o input do nome do usuario
 print(f'É um prazer te conhecer, \033[4;31m{nome}\033[m!') #print com o nome que o usuario informou
