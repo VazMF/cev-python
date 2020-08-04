3a3d1827fef20a3d17cef70195818a2072a32aef
vaz-mf <vazfernandam@gmail.com>
2020-08-04 14:28:03 -0300

    Quebrando um número

--- PythonExercicio/ex016.py
+++ PythonExercicio/ex016.py
@@ -2,4 +2,4 @@
 from math import trunc #import do trunc
 print('\033[35m-------PARTE INTEIRA-------\033[m') #titúlo
 num = float(input('Digite um número real: ')) #input do número real na variavel num
-print(f'A parte inteira de \033[35m{num}\033[m é \033[35m{trunc(num)}\033[m') #pegando a parte inteira do input por meio da função trunc e mostrando o resultado
+print(f'A parte inteira de \033[35m{num}\033[m é \033[35m{trunc(num)}\033[m') #pegando a parte inteira do input por meio da função trunc e mostrando o resultado na tela
