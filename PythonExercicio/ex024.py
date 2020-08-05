9ed353b50c4264d517f985b3c799c7b910fdf39b
vaz-mf <vazfernandam@gmail.com>
2020-08-05 13:53:04 -0300

    Verificando as primeiras letras de um texto

--- PythonExercicio/ex024.py
+++ PythonExercicio/ex024.py
@@ -1,8 +1,9 @@
 #crie um programa que leia o nome de uma cidade e diga se ela começa com o nome 'SANTO'.
 from time import sleep #import do sleep
-print('\033[31m---COMEÇA COM SANTO?---\033[m') #titúlo
-cid = str(input('Digite o nome da cidade: ')).strip() #input do nome da cidade na variavel cid
+print('\033[31m---------------------------------------------------\033[m') #titúlo
+cid = str(input('Nome da cidade: ')).strip() #input do nome da cidade na variavel cid
 print(f'A cidade de \033[31m{cid}\033[m começa com a palavra Santo?') #print formatado
 print('\033[31mANALISANDO...\033[m') #simulacao loading
 sleep(1) #sleep
-print(cid[:5].upper() == 'SANTO') #procura santo no input
+print(f'Resultado: {cid[:5].upper() == "SANTO"}') #procura santo no input
+print('\033[31m---------------------------------------------------\033[m')
