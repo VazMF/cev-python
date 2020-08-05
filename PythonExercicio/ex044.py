643c6606517dd71106174c87dbc16c280c8b89b7
vaz-mf <vazfernandam@gmail.com>
2020-08-05 15:50:48 -0300

    Gerenciador de Pagamentos

--- PythonExercicio/ex044.py
+++ PythonExercicio/ex044.py
@@ -1,6 +1,6 @@
 #Elabore um programa que calcule o valor a ser pago por um produto. considerando seu preço normal e condição de pagamento.
 #à vista/dinheiro/cheque = 10% de desconto; à vista no cartão 5% de desconto; em até 2x no cartão preço normal; 3x ou mais no cartão = 20% de juros.
-print('\033[35m===================CALCULANDO PREÇO===================\033[m') #titulo
+print('\033[35m====================CALCULANDO PREÇO====================\033[m') #titulo
 preço = float(input('Preço das compras: R$')) #input do preço do produto
 print('''FORMAS DE PAGAMENTO
 [ 1 ] à vista dinheiro/cheque
