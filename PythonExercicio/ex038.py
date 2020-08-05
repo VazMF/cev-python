d69eef2f41fecda70cbefc73740817a45dbc4422
vaz-mf <vazfernandam@gmail.com>
2020-08-05 15:29:51 -0300

    Comparando Números

--- PythonExercicio/ex038.py
+++ PythonExercicio/ex038.py
@@ -1,5 +1,5 @@
 #Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem: o primeiro valor é maior; o segundo valor é maior; são iguais.
-print('---COMPARANDO VALORES---') #titúlo
+print('-----COMPARANDO VALORES-----') #titúlo
 n1 = int(input('Primeiro número inteiro: ')) #input do primeiro valor na variavel n1
 n2 = int(input('Segundo número inteiro: ')) #input do segundo valor na variavel n2
 if n1 > n2: #se n1 for maior que n2 execute o comando abaixo e termine
@@ -8,4 +8,4 @@ elif n2 > n1: #se n2 for maior que n1 execute o comando abaixo e termine
     print('O segundo valor é \033[33mMAIOR\033[m.')
 else: #se nenhuma das afirmações anteriores forem verdadeiras
     print('Os dois valores são \033[33mIGUAIS\033[m') #os números sao iguais
-print('-' * 24) #fim
+print('-' * 28) #fim
