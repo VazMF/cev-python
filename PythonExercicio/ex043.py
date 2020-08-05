3705e934a278fa88fb890a6c2935755094cbf265
vaz-mf <vazfernandam@gmail.com>
2020-08-05 15:46:44 -0300

    Índice de Massa Corporal

--- PythonExercicio/ex043.py
+++ PythonExercicio/ex043.py
@@ -1,6 +1,6 @@
 #Dsenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status de acordo com a tabela abaixo:
 #abaixo de 18,5 = abaixo do peso; entre 18,5 e 25 = peso ideal; 25 até 30 = sobrepeso; acima de 40 = obesidade mórbida.
-print('\033[31m=-=IMC=-=\033[m') #titúlo
+print('\033[31m------------IMC------------\033[m') #titúlo
 peso = float(input('Quantos é seu peso? (kg) ')) #input do peso
 altura = float(input('Qual é sua altura? (m) ')) #input da altura
 imc = peso / (altura**2) #cálcula o imc
@@ -15,4 +15,4 @@ elif 30 <= imc < 48:
     print('Você está em \033[1mOBESIDADE\033[m')
 elif imc >= 40: #se o imc não se encaixar em nenhuma das outras alternativas
     print('Você está em \033[1mOBESIDADE MÓRBIDA\033[m.') #print obesidade mórbida
-print('\033[31m=-=\033[m' * 3) #fim
+print('\033[31m-\033[m' * 27) #fim
