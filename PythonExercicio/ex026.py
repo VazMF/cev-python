9d8158fc7a2a52306b5c284d2213843bf6cca3d8
vaz-mf <vazfernandam@gmail.com>
2020-08-05 14:00:47 -0300

    Primeira e última ocorrência de uma string

--- PythonExercicio/ex026.py
+++ PythonExercicio/ex026.py
@@ -1,6 +1,6 @@
 #faça um programa que leia uma frase e mostre quantas vezes aparece a letra A, em que posição ela aparece na 1 vez e em que posição aparece na última vez
 from time import sleep #import do sleep
-print('\033[35m---PRIMEIRA E ÚLTIMA OCORRÊNCIA---\033[m') #titúlo
+print('\033[35m------PRIMEIRA E ÚLTIMA OCORRÊNCIA------\033[m') #titúlo
 frase = str(input('Digite uma frase: ')).strip().upper() #variavel da frase para input jogada pra upper
 print('\033[35mPROCESSANDO...\033[m') #print para simular processamento
 sleep(1) #sleep
