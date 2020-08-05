5066245dc5da489dcc8686693f42b29b74fda324
vaz-mf <vazfernandam@gmail.com>
2020-08-05 15:28:29 -0300

    Conversor de Bases Numéricas

--- PythonExercicio/ex037.py
+++ PythonExercicio/ex037.py
@@ -1,5 +1,5 @@
 #Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário; 2 para octal e 3 para hexadecimal
-print('\033[31m=-=CONVERTENDO=-=\033[m') #titulo
+print('\033[32m--------------CONVERTENDO--------------\033[m') #titulo
 num = int(input('Digite um número inteiro: ')) #input do numero da variavel num
 print('''Escolha uma das bases para conversão:
 [ 1 ] converter para \033[32mBINÁRIO\033[m
@@ -14,4 +14,4 @@ elif opção == 3: #se a opcao for 3
     print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}') #print do numero convertido para hexadecimal por meio do hex
 else: #senao
     print('\033[31mOPÇÃO INVÁLIDA. TENTE NOVAMENTE.\033[m') #print da opcao invalida
-print('\033[31m=-=\033[m' * 6)
+print('\033[32m-\033[m' * 40)
