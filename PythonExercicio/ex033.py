1bae07dda20e5ce5fd763dfd563cfbe6eecf82d8
vaz-mf <vazfernandam@gmail.com>
2020-08-05 15:08:03 -0300

    Maior e menor valores

--- PythonExercicio/ex033.py
+++ PythonExercicio/ex033.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia três números e mostre qual1 é o maior e qual é o menor.
 from time import sleep
-print('\033[31m---MAIOR E MENOR---\033[m') #titúlo
+print('\033[31m--------MAIOR E MENOR--------\033[m') #titúlo
 n1 = int(input('Digite o primeiro número: ')) #input do primeiro numero na variavel n1
 n2 = int(input('Digite o segundo número: ')) #input do segundo numero na avriavel n2
 n3 = int(input('Digite o terceiro número: ')) #input do terceiro numero na variavel n3
@@ -20,4 +20,4 @@ if n1 > n2 and n1 > n3: #se n1 for maior que n2 e n3
 if n3 > n2 and n3 > n1: #se n3 for maior que n2 e n1
     maior = n3 #a variavel 'maior' será substituida por n3
 print(f'O \033[4;33mmaior\033[m número digitado foi {maior}.') #mostra o maior número do input
-print('\033[31m-\033[m' * 19) #fim
+print('\033[31m-\033[m' * 29) #fim
