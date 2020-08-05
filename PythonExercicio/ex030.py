a0e82e108c8fe9f79b18aa4300ec9002761beb2a
vaz-mf <vazfernandam@gmail.com>
2020-08-05 14:40:03 -0300

    Par ou ímpar

--- PythonExercicio/ex030.py
+++ PythonExercicio/ex030.py
@@ -2,7 +2,7 @@
 from time import sleep
 print('\033[33m---PAR OU IMPÁR?---\033[m')
 n = int(input('Digite um número: ')) #input do número digitado pelo usuário na variavel n
-print('\033[33mPROCESSANDO...\033[m')
+print('\033[33mANALISANDO...\033[m')
 sleep(1)
 if n % 2 == 0: #se o resto da divisao por dois for zero o numero é par
     print(f'O número {n} é \033[4;33mPAR\033[m.') #mensagem que aparece caso o número for par
