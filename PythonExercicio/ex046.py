0888b3d9546dd2f31bdbf4d75d4040efe7887b08
vaz-mf <vazfernandam@gmail.com>
2020-08-10 13:41:45 -0300

    Contagem regressiva

--- PythonExercicio/ex046.py
+++ PythonExercicio/ex046.py
@@ -3,4 +3,4 @@ from time import sleep
 for cont in range(10, -1, -1): #para cont de 10 ate -1 regredindo 1
     print(cont) #print conteudo da variavel cont
     sleep(0.5) #sleep por meio segundo
-print('\033[33m**BOOM! BOOM! BOOM!**\033[m') #print dos 'fogos'
+print('\033[33m***BOOM! BOOM! BOOM!***\033[m') #print dos 'fogos'
