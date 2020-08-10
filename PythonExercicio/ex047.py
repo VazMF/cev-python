38688ebc2f8f2d911e7766f1b2ced0127fe19f56
vaz-mf <vazfernandam@gmail.com>
2020-08-10 13:43:44 -0300

    Contagem de Pares

--- PythonExercicio/ex047.py
+++ PythonExercicio/ex047.py
@@ -1,4 +1,4 @@
 #crie um programa que mostre na tela todos os número pares que estão no intervalo entre 1 e 50
 for n in range(2, 51, 2): #para n de 2 ate 51 pulando de dois em dois
-    print(n, end=' ') #print n e nao quebre a linha
+    print(f'{n} ->', end=' ') #print n e nao quebre a linha
 print('FIM') #fim
