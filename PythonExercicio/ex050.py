61952e9d52d6dd13ea7bd926e8fa59f5e34d9f35
vaz-mf <vazfernandam@gmail.com>
2020-08-10 13:59:29 -0300

    Soma dos Pares

--- PythonExercicio/ex050.py
+++ PythonExercicio/ex050.py
@@ -1,5 +1,5 @@
 #Desenvolva um programa que leia 6 números inteiros e mostre a soma apenas daqueles que forem pares.
-print('\033[31m-=SOMA-DOS-PARES=-\033[m')
+print('\033[31m-----------------SOMA-DOS-PARES----------------\033[m')
 soma = cont = 0 #declaracao das variaveis
 for c in range(1, 7): #repita de 1 ate 7
     num = int(input(f'Digite o {c}° valor: ')) #input do numero na variavel num
@@ -7,4 +7,4 @@ for c in range(1, 7): #repita de 1 ate 7
         soma += num #soma recebe soma mais num
         cont += 1 #cont recebe cont mais um
 print(f'Você informou {cont} números PARES e a soma foi {soma}.') #print formatado com o resultado
-print('\033[31m-=-\033[m' * 6)
+print('\033[31m-\033[m' * 47)
