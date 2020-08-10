a03b40a1579bbc53f64b40811b4d3af2b366ae60
vaz-mf <vazfernandam@gmail.com>
2020-08-10 14:01:33 -0300

    Progressão Aritmética

--- PythonExercicio/ex051.py
+++ PythonExercicio/ex051.py
@@ -1,5 +1,5 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. E no final, mostre a soma dos 10 primeiros termos da progressão.
-print('\033[31m-=10-TERMOS-DE-UMA-PA=-\033[m') #titulo
+print('\033[33m-=10-TERMOS-DE-UMA-PA=-\033[m') #titulo
 primeiro = int(input('Primeiro termo: ')) #input do primeiro termo da PA
 razão = int(input('Razão: ')) #input da razao da PA
 decimo = primeiro + (10 - 1) * razão #formula para calcular o decimo termo da PA
