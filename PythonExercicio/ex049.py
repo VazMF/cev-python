c0019299f3277de522be2c2c0626229fecdf2b2e
vaz-mf <vazfernandam@gmail.com>
2020-08-10 13:55:58 -0300

    Tabuada v2.0

--- PythonExercicio/ex049.py
+++ PythonExercicio/ex049.py
@@ -1,4 +1,6 @@
 #refaça o desafio 009, motrando a tabuada de um número que o usuário escolher só que agora utilizando um laço for.
+print('\033[34m-\033[m' * 40)
 num = int(input('Digite um número para ver sua tabuada: ')) #input do numero na variavel num
 for c in range(1, 11): #repita de 1 ate 10
     print(f'{num} \033[34mx\033[m {c:2} \033[34m=\033[m {num*c}') #print formatado
+print('\033[34m-\033[m' * 40)
