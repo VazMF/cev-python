ae987f4b51031bd0be28a5e8ae47a4cb4b2bad7c
vaz-mf <vazfernandam@gmail.com>
2020-08-14 14:25:50 -0300

    Jogo da Adivinhação v2.0

--- PythonExercicio/ex058.py
+++ PythonExercicio/ex058.py
@@ -1,8 +1,9 @@
 #melhore o jogo do desafio 028, onde o computador vai pensar em um número  entre 0 e 10. só que agora o jogador vai tentar adivinhar até acertar. mostrando no final quantos palpites foram necessários para vencer.
 from random import randint #import do randint para randomizar a escolha do computador
-print('\033[31m=-=JOGO-DA-ADIVINHAÇÃO=-=\033[m') #titulo
+print('\033[35m---------------JOGO-DA-ADIVINHAÇÃO 2.0---------------\033[m') #titulo
 comp = randint(0, 10) #computador escolhe um numero aleatorio de 0 a 10
-print('Acabei de pensar em um número entre 0 e 10. Será que você consegue adivinhar?')
+print('Acabei de pensar em um número entre 0 e 10...')
+print('Será que você consegue adivinhar?')
 acertou = False #determina que ngm acertou
 tentativas = 0
 while not acertou: #enquanto ngm tiver acertado
@@ -11,9 +12,9 @@ while not acertou: #enquanto ngm tiver acertado
         acertou = True #altere acertou para verdadeiro
     else: #caso contrario
         if jogador < comp: #se a respota do jogador for menor que a do computador
-            print('Mais... Tente novamente.') #printa para ele chutar um numero maior
+            print('\033[35mMais... Tente novamente.\033[m') #printa para ele chutar um numero maior
         elif jogador > comp: #se a respota do jogador for maior que a do computador
-            print('Menos... Tente novamente.') #printa para ele chutar um numero menor
+            print('\033[35mMenos... Tente novamente.\033[m') #printa para ele chutar um numero menor
     tentativas += 1 #adiciona uma tentativa a cada jogada, ate o jogador acertar
-print(f'\033[33mParabéns!\033[m Eu realmente pensei no número {comp}.')
+print(f'\033[32mParabéns! Eu realmente pensei no número {comp}.\033[m')
 print(f'Você precisou de {tentativas} tentativas para acertar.')
