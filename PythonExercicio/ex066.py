7035e999c824c111ec4fa4fd09fd87285215a2d8
vaz-mf <vazfernandam@gmail.com>
2020-08-14 15:07:41 -0300

    Vários números com flag

--- PythonExercicio/ex066.py
+++ PythonExercicio/ex066.py
@@ -1,5 +1,6 @@
 #crie um programa que leia vários números inteiros pelo teclado. o programa só vai parar quando o usuário didgitar 999, que é a condição de parada. no final, mostre quantos números
 #foram digitados e qual foi a soma entre eles (desconsiderando o flag)
+print('-' * 45)
 cont = soma = 0 #inicializacao das variaveis contador e soma como 0
 while True: #loop infinito
     n = int(input('Digite um número [999 para parar]: ')) #input do numero
@@ -9,3 +10,4 @@ while True: #loop infinito
     cont += 1 #contador soma mais um
     soma += n #soma recebe soma mais n
 print(f'A soma dos {cont} valores foi {soma}!') #resultado
+print('-' * 45)
