6c174d6f67916e82156a50b4c50e895c3f6836b5
vaz-mf <vazfernandam@gmail.com>
2020-08-14 14:11:51 -0300

    Maior e menor na sequência

--- PythonExercicio/ex055.py
+++ PythonExercicio/ex055.py
@@ -1,11 +1,10 @@
 #Faça um programa que leia o peso de cinco pessoas e no final mostre qual foi o maior e o menor peso lido.
-print('\033[31m=-=MAIOR-E-MENOR-PESO=-=\033[m') #titulo
+print('\033[33m=-=-=-=MAIOR-E-MENOR-PESO=-=-=-=\033[m') #titulo
 maior = menor = 0 #inicializacao das variaveis maior e menor
 for p in range(1, 6): #repeticao de 1 ate 5
     peso = float(input(f'Peso da {p}º pessoa: ')) #variavel peso recebe o input do usuario
     if p == 1: #se for a primeira repeticao
-        maior = peso #maior recebe o primeiro peso
-        menor = peso #menor recebe o primeiro peso
+        maior = menor = peso #maior recebe o primeiro peso
     else: #senao for a primeira repeticao
         if peso > maior: #se o valor da variavel peso for maior que o valor que ja esta armazenado na variavel maior
             maior = peso #maior recebe o valor da variavel peso
