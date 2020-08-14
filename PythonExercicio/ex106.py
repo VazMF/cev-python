2eef2f69f83cf9957ed59927b67fd84eb1e98b15
vaz-mf <vazfernandam@gmail.com>
2020-08-14 20:55:08 -0300

    Interactive helping system in Python

--- PythonExercicio/ex106.py
+++ PythonExercicio/ex106.py
@@ -1,4 +1,4 @@
-#Faça um mini sistema que utilize o interactive help do python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM' o programa será encerrado. OBS: Use cores.
+#Faça um mini sistema que utilize o interactive help do python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM' o programa será encerrado.
 from time import sleep
 #DEFININDO A FUNÇÃO
 def ajuda(cmd): #define a função ajuda com o parâmetro cmd
@@ -9,9 +9,9 @@ def ajuda(cmd): #define a função ajuda com o parâmetro cmd
 
 def titulo(txt): #define a função tuitulo com o parâmetro txt
     tam = len(txt) + 4 #tamanho recebe o len de txt mais 4
-    print('~' * tam) #print ~ vezes o tamanho
+    print('-' * tam) #print ~ vezes o tamanho
     print(f'  {txt}') #print formatado com o texto
-    print('~' * tam) #print ~ vezes o tamanho
+    print('-' * tam) #print ~ vezes o tamanho
     sleep(1)
 
 
