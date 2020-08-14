603ad3205c6a937fe52d6f58bd5b66e4842c7776
vaz-mf <vazfernandam@gmail.com>
2020-08-14 14:00:30 -0300

    Grupo da Maioridade

--- PythonExercicio/ex054.py
+++ PythonExercicio/ex054.py
@@ -1,5 +1,5 @@
 #Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maior idade e quantas já são maiores.
-print('\033[31m===MAIORIDADE===\033[m') #titulo
+print('\033[31m===============MAIORIDADE===============\033[m') #titulo
 from datetime import date 
 atual = date.today().year #variavel atual recebe a data atual do computador
 totmaior = totmenor = 0 #inicializacao das variaveis totmaior e totmenor para armazenar os maiores e menores de idade
@@ -12,4 +12,3 @@ for pess in range(1, 8): #repeticao para cada pessoa de 1 ate 7
         totmenor += 1 #total de menores recebe mais um
 print(f'Ao todo tivemos {totmaior} pessoas maiores de idade.') #print formatado com a quantidade de pessoas maiores
 print(f'E também tivemos {totmenor} pessoas menores de idade.') #print formatado com a quantidade de pessoa menores
-print('\033[31m=\033[m' * 16)
