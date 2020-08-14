28f9a5980584607144510f3950cd85e1885692e6
vaz-mf <vazfernandam@gmail.com>
2020-08-14 20:25:06 -0300

    Funções para sortear e somar

--- PythonExercicio/ex100.py
+++ PythonExercicio/ex100.py
@@ -1,4 +1,4 @@
-#Faça um programq eu tenha uma lista chamada números e duas funções chamadas sorteio() e somaPar(). A primeira função vai sortear 5 números e va colocá-los dentro da lista, a segunda função vai mostrar a soma entre todos os valores PARES sorteados pela função anterior.
+#Faça um programq eu tenha uma lista chamada números e duas funções chamadas sorteio() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista, a segunda função vai mostrar a soma entre todos os valores PARES sorteados pela função anterior.
 from random import randint
 from time import sleep
 #DEFININDO A FUNÇÃO
